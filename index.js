047223b2a00597aaeff65b5e2abf6e8d1411b1bf
Mike Griffith <bikegriffith@gmail.com>
2019-06-20 14:43:17 -0400

    export BroadcastModalView

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Broadcast from './src/Broadcast';
+import BroadcastModalView from './src/BroadcastModalView';
 import ChannelList from './src/ChannelList';
 
 export {
   Broadcast,
+  BroadcastModalView,
   ChannelList,
 };
